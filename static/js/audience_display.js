024bd87339e06b07798e2ac9a686a8c8204884eb
Jeff S <jwspragg@mtu.edu>
2021-10-20 01:36:51 -0700

    Space out team number display when there are no ranks

--- static/js/audience_display.js
+++ static/js/audience_display.js
@@ -452,7 +452,7 @@ var getAvatarUrl = function(teamId) {
 var getRankingText = function(teamId, rankings) {
   var ranking = rankings[teamId];
   if (ranking === undefined || ranking.Rank === 0) {
-    return "";
+    return "<div class='rank-spacer'></div>";
   }
 
   if (ranking.Rank > ranking.PreviousRank && ranking.PreviousRank > 0) {
